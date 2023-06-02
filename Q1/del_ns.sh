686fdbdc8aa7daaa3edea13caf4b30f5379cdbc0
ahmad-karami <ahmad.karami77@yahoo.com>
2023-06-02 22:28:32 +0330

    Q1_imp

---
+++ Q1/del_ns.sh
@@ -0,0 +1,20 @@
+ip netns del ns1
+ip netns del ns2
+ip netns del ns3
+ip netns del ns4
+ip netns del router
+
+ip link set br1 down
+brctl delbr br1
+ip link set br2 down
+brctl delbr br2
+ip link set br3 down
+brctl delbr br3
+ip link set br4 down
+brctl delbr br4
+ip link delete veth_nsr_br1
+ip link delete veth_nsr_br2
+ip link delete veth_nsr_br3
+ip link delete veth_nsr_br4
+#ip link delete veth_ns1_br1
+echo "all del"
---
+++ Q1/ping.sh
@@ -0,0 +1,77 @@
+echo "ping from $1 to $2"
+if [ "$1" = "node1" ]
+then
+	if [ "$2" = "router" ]
+	then 
+	 ip netns exec ns1 ping 172.0.0.1 -c4
+	fi 
+	if [ "$2" = "node2" ]
+	then 
+	 ip netns exec ns1 ping 172.0.0.3 -c4
+	fi 	
+	if [ "$2" = "node3" ]
+	then 
+	 ip netns exec ns1 ping 10.10.0.2 -c4
+	fi 
+	if [ "$2" = "node4" ]
+	then 
+	 ip netns exec ns1 ping 10.10.0.3 -c4
+	fi	   
+fi
+if [ "$1" = "node2" ]
+then
+	if [ "$2" = "router" ]
+	then 
+	 ip netns exec ns2 ping 172.0.0.1 -c4
+	fi 
+	if [ "$2" = "node1" ]
+	then 
+	 ip netns exec ns2 ping 172.0.0.2 -c4
+	fi 	
+	if [ "$2" = "node3" ]
+	then 
+	 ip netns exec ns2 ping 10.10.0.2 -c4
+	fi 
+	if [ "$2" = "node4" ]
+	then 
+	 ip netns exec ns2 ping 10.10.0.3 -c4
+	fi	   
+fi
+if [ "$1" = "node3" ]
+then
+	if [ "$2" = "router" ]
+	then 
+	 ip netns exec ns3 ping 10.10.0.1 -c4
+	fi 
+	if [ "$2" = "node1" ]
+	then 
+	 ip netns exec ns3 ping 172.0.0.2 -c4
+	fi
+	if [ "$2" = "node2" ]
+	then 
+	 ip netns exec ns3 ping 172.0.0.3 -c4
+	fi 	
+	if [ "$2" = "node4" ]
+	then 
+	 ip netns exec ns3 ping 10.10.0.3 -c4
+	fi	   
+fi
+if [ "$1" = "node4" ]
+then
+	if [ "$2" = "router" ]
+	then 
+	 ip netns exec ns4 ping 10.10.0.1 -c4
+	fi 
+	if [ "$2" = "node1" ]
+	then 
+	 ip netns exec ns4 ping 172.0.0.2 -c4
+	fi
+	if [ "$2" = "node2" ]
+	then 
+	 ip netns exec ns4 ping 172.0.0.3 -c4
+	fi 	
+	if [ "$2" = "node3" ]
+	then 
+	 ip netns exec ns4 ping 10.10.0.2 -c4
+	fi	   
+fi
